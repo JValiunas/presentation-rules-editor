7dab543de4814f66f93d8c6bdb9645b39ad7b560
Jonas.Valiunas <jonas.valiunas@bentley.com>
2022-02-21 14:40:59 +0200

    display data in table format

--- app/frontend/src/app/ITwinJsApp/widgets/AssociatedElementsWidget.tsx
+++ app/frontend/src/app/ITwinJsApp/widgets/AssociatedElementsWidget.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { KeySet, Ruleset } from "@itwin/presentation-common"
+import { InstanceKey, KeySet, NodeKey, Ruleset } from "@itwin/presentation-common"
 import { RelatedElementIdsProvider } from "./RelatedElementIdsProvider";
 import { IModelConnection } from "@itwin/core-frontend";
+import { Table } from "@itwin/itwinui-react";
 
 
 export interface AssociatedElementsWidgetProps {
@@ -27,7 +28,65 @@ export function AssociatedElementsWidget(props: AssociatedElementsWidgetProps): 
     console.log(JSON.stringify(ex));
   }
 
-  return <>
-    {JSON.stringify(relatedNodes)}
-  </>;
+const columnsDescription = React.useMemo(() => {
+  return {
+      Header: 'Associated Elements',
+      columns: [
+        {
+          Filter: function noRefCheck(){},
+          Header: 'Class Name',
+          accessor: 'classname',
+          id: 'classname'
+        },
+        {
+          Header: 'Count',
+          accessor: 'count',
+          id: 'count'
+        },
+        {
+          Header: 'ECInstanceId',
+          accessor: "ecinstanceid",
+          id: 'ecinstanceid'
+        }
+      ]
+    }
+
+}, []);
+
+const data = React.useMemo(() => {
+  const computedObject: Record<string, unknown>[] = [];
+  relatedNodes.instanceKeys.forEach( (ecinstanceids, className) => {
+    const entries: Record<string, unknown>[] = [];
+    for (const entry of ecinstanceids){
+      entries.push({
+        classname: className,
+        ecinstanceid: entry,
+        subRows: []
+      });
+    }
+
+    computedObject.push({
+      classname: className,
+      count: ecinstanceids.size,
+      subRows: entries
+    });
+  });
+  return computedObject;
+}, [relatedNodes]);
+
+return <Table
+    columns={[
+      columnsDescription
+    ]}
+    data={
+      data
+    }
+    density="default"
+    emptyFilteredTableContent="No results found. Clear or try another filter."
+    emptyTableContent="No data."
+    isSortable
+    isResizable
+    onExpand={function noRefCheck(){}}
+  />;
+
 };
